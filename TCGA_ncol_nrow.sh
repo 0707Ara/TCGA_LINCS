a8d54dd4f47b37138afb48482ea6d2382e0532c8
0707Ara <hyj1722@gmail.com>
2021-07-06 17:38:34 +0900

    results_modified

--- TCGA_ncol_nrow.sh
+++ TCGA_ncol_nrow.sh
@@ -11,23 +11,26 @@ for file in *; do
 	cd $file
 		for f in *; do
 			if [[ $f == *.gz ]]; then
+				#extract number of rows and columns from .gz file and write to nrow.txt file
 				ncol_origin=`zcat $f | head -n 1 | tr $'\t' '\n' | wc -l`
                                 nrow_origin=`zcat $f | wc -l`
-                                arr+=("$f")
-                                arr+=("$ncol_origin")
-                                arr+=("$nrow_origin")
+                                echo -n $f >>/home/arajo0707/TCGA_LINCS/nrow.txt
+                                echo -e -n ' \t ' >>/home/arajo0707/TCGA_LINCS/nrow.txt
+				echo -n $ncol_origin>>/home/arajo0707/TCGA_LINCS/nrow.txt
+                                echo -e -n ' \t '>>/home/arajo0707/TCGA_LINCS/nrow.txt
+				echo -n $nrow_origin>>/home/arajo0707/TCGA_LINCS/nrow.txt
+				echo -e -n ' \t '>>/home/arajo0707/TCGA_LINCS/nrow.txt
 			elif [[ $f == *.txt ]]; then
+				#extract number of rows and columns from .txt file and write to nrow.txt file
 				ncol_sub=`awk '{print NF}' $f | sort -nu | tail -n 1`
 				nrow_sub=`wc -l < $f`
-				arr+=("$f")
-				arr+=("$ncol_sub")
-				arr+=("$nrow_sub")
+				echo -n $f >>/home/arajo0707/TCGA_LINCS/nrow.txt
+                                echo -e -n ' \t ' >>/home/arajo0707/TCGA_LINCS/nrow.txt
+                                echo -n $ncol_sub>>/home/arajo0707/TCGA_LINCS/nrow.txt
+                                echo -e -n ' \t ' >>/home/arajo0707/TCGA_LINCS/nrow.txt
+                                echo -n $nrow_sub>>/home/arajo0707/TCGA_LINCS/nrow.txt
+				echo -e -n ' \t ' >>/home/arajo0707/TCGA_LINCS/nrow.txt
 			fi
 		done
-
 	cd ../
-done
-
-for value in "${arr[@]}";do
-	echo $value >>/home/arajo0707/TCGA_LINCS/temp.txt
 done
